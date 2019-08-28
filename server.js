12f120e9986dba4ceab9b6a3861ff51c7d3196df
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-28 18:02:28 -0400

    Required our two routing files

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
