9e7d4e6c2e4ed5ed64ae54f92c7becd3fc0c513f
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-28 17:25:25 -0400

    Corrected psuedo labels for routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,9 @@
-// Basic route that sends the user first to the AJAX Page
+//Adding GET route for survey page
 app.get("/survey", function(req,res){
     res.sendFile(path.join(__dirname,"survey.html"));
 })
 
+//Default catch-all route to home page
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "home.html"));
 })
