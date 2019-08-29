ec34e368737f0b6a97cbb57f4977ed21c61de1e6
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-29 18:21:48 -0400

    Corrected routes in the htmlRoutes.js file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,15 +2,15 @@ var path = require("path");
 //Adding GET route for survey page
 module.exports = function (app) {
     app.get("/home", function (req, res) {
-        res.sendFile(path.join(__dirname, "app/public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "app/public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     //Default catch-all route to home page
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "app/public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     })
 };
