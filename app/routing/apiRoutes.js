0418a5fcfb884e311e83f73ddfb5444c2cdf785f
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-29 16:02:04 -0400

    Moved both app.get method and app.post method within our function to pass through the data within our app folder.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,13 +8,16 @@ function compare(user, friend){
     return difference;
 }
 
-
-//GET route to display all possible friends
+module.exports = function (app) {
+ //GET route to display all possible friends
 app.get("/api/friends", function(req,res){
     return res.json(friends);
-})
-
+}); 
 //POST route to handle incoming survey results and compatibility logic
 app.post("/api/friends", function(req,res){
    
-})
+});
+
+};
+
+
