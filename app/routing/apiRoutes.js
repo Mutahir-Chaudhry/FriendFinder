128bc6e5be6d4cc4e6b543220678f72da35415a9
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-29 16:13:46 -0400

    Since our app.get is already complete, within the app.post method, created 3 new variables. newFriend variable to hold the json returned, bestScore to create a max score to compare the user score to, and our match variable to store the best match for our user. We find the best match by running a for loop for the length of our existing friends array, and pass through our user's (newFriend's) score and compare it to each existing friends score. Who ever has the closest score to our newFriend, will be assigned to the match variable, and the best score will be set to our user's score.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-const friendData = require("../data/friends");
+const friends = require("../data/friends");
 
 function compare(user, friend){
     const difference = 0;
@@ -15,6 +15,18 @@ app.get("/api/friends", function(req,res){
 }); 
 //POST route to handle incoming survey results and compatibility logic
 app.post("/api/friends", function(req,res){
+
+    const newFriend = req.body;
+    const bestScore = 101;
+    const match;
+
+    for(var i = 0; i < friends.length; i++){
+        var score = compare(newFriend.scores, friends[i].scores);
+        if(score < bestScore){
+            match = friends[i];
+            bestScore = score;
+        }
+    }
    
 });
 
