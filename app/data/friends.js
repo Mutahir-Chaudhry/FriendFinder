650c0bfdcca7e46ca804da418594c2aaa67d0358
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-31 12:34:56 -0400

    Trying to add ipsum images for friends list.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         name: "Jessica",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [2,1,4,4,5,1,3,5,4,1]
     },
     {
         name: "Hammy",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [4,2,5,4,5,1,2,4,3,2]
     },
     {
         name: "Maaz",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [3,1,2,3,2,1,4,1,2,4]
     },
     {
         name: "Ahmed",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [5,2,4,1,3,1,5,2,4,5]
     },
     {
         name: "Jenny",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [1,1,4,2,5,4,2,3,1,2]
     },
     {
         name: "Emily",
-        photo: "",
+        photo: "https://picsum.photos/250",
         scores: [5,1,4,4,5,1,2,5,5,3]
     },
 ]
